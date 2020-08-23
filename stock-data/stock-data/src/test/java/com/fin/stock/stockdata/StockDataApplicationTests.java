c59b3acb15445c336f60bed7eb09e6a74708b3fe
Mohit Tandon <tmohit@gmail.com>
2020-08-23 17:58:14 +0530

    Initial commit

---
+++ stock-data/stock-data/src/test/java/com/fin/stock/stockdata/StockDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fin.stock.stockdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
